af6b32714156cf85e3addfb51934b6df0d98861b
Filip3ra <filipimacielrj@gmail.com>
2024-09-02 10:46:40 -0300

    dijkstra ok, falta gerar grandes instancias

--- Codigo/Dijkstra/main.cpp
+++ Codigo/Dijkstra/main.cpp
@@ -1,21 +1,33 @@
 #include "header.hpp"
+#include <iostream>
+#include <chrono> // Para medir o tempo de execução
 
 using namespace std;
+using namespace std::chrono;
 
-int main()
+int main(int argc, char *argv[])
 {
-    // Número de vértices no grafo
-    int vertices = 5;
+    // Verificar se os dois argumentos (número de nós e nome do arquivo) foram passados
+    if (argc < 3)
+    {
+        cerr << "Uso: " << argv[0] << " <numero_de_nos> <nome_do_arquivo>" << endl;
+        return 1;
+    }
 
-    // Criação do grafo
+    // Converte o primeiro argumento (quantidade de nós) para um inteiro
+    int vertices = stoi(argv[1]);
+
+    string filename = argv[2];
+
     Graph graph(vertices);
 
-    // Leitura das arestas a partir de um arquivo
-    readGraphFromFile("e1.txt", graph);
+    readGraphFromFile(filename, graph);
 
-    // Execução do algoritmo de Dijkstra
     int startVertex = 0; // Vértice inicial
+
+    auto start_time = high_resolution_clock::now(); // Marca tempo inicial
     vector<int> distances = graph.dijkstra(startVertex);
+    auto end_time = high_resolution_clock::now(); // Tempo final
 
     // Exibição das distâncias mínimas a partir do vértice inicial
     for (int i = 0; i < vertices; ++i)
@@ -30,6 +42,10 @@ int main()
             cout << distances[i] << endl;
         }
     }
+
+    // Calcula a duração em milissegundos
+    auto duration = duration_cast<milliseconds>(end_time - start_time);
+    cout << "Tempo de execucao do algoritmo: " << duration.count() << " milissegundos" << endl;
 
     return 0;
 }
